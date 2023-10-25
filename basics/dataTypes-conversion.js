4c79763819aace90a35577152bd5b3857bb366f4
MianAteeq-star <142322826+MianAteeq-star@users.noreply.github.com>
2023-10-25 14:57:19 +0000

    confution_dataTypes

---
+++ basics/dataTypes-conversion.js
@@ -0,0 +1,12 @@
+// let score =null
+// let number = Number(score)
+// console.log(number)
+
+// let bolean = undefined
+// let toBolean = Boolean(bolean)
+// console.log(toBolean);
+
+// let mail= null
+// let string= String(mail)
+// console.log( typeof string);
+// console.log(  string);
