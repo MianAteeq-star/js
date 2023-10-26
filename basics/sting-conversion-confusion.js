45421090ab1a89363f374cd19aeadfef444e2465
MianAteeq-star <142322826+MianAteeq-star@users.noreply.github.com>
2023-10-26 04:36:45 +0000

    string confusion

---
+++ basics/sting-conversion-confusion.js
@@ -0,0 +1,19 @@
+// let str1 ="mian"
+// let str2 =" g"
+// const str3= str1+str2
+// console.log(str3);
+
+// console.log('1'+2+3);
+// console.log(1+'2'+3);
+// console.log(1+2+'2');
+// console.log(2+3*5%2);
+
+// console.log(+"");
+
+// let n1,n2,n3
+// n1=n2 = n3= 3-2
+// console.table([n1,n3,n2]);
+let counter= 103
+counter--;
+console.log(counter);
+
