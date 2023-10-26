044820e0ad223470ea16a9d8b2b09dfb02cdd9cf
MianAteeq-star <142322826+MianAteeq-star@users.noreply.github.com>
2023-10-26 16:12:28 +0000

    stringMethod

--- basics/stringMethod.js
+++ basics/stringMethod.js
@@ -2,7 +2,7 @@ const stringOne = "Mian"
 // const count = 34
 // console.log(stringOne + count +" g");
 // console.log(`My name is ${stringOne}  g and the value of count is ${count} `);
-const newString = new String("Mian-M-Ateeq")
+const newString = new String("     Mian-M-Ateeq")
 // console.log(newString[2]);
 console.log(newString.length);
 // console.log(newString.slice(0,3));
@@ -15,3 +15,7 @@ console.log(newString);
 // console.log(newString.anchor("ab"));
 // console.log(newString.charAt(1));
 // console.log(newString.indexOf("M"));
+
+console.log(newString.toUpperCase());
+console.log(newString.replaceAll(" ",2));
+console.log(newString.includes("Mian"));
