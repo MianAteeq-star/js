9e56e0ee2cf96c800d144a636757899dff2bc850
MianAteeq-star <142322826+MianAteeq-star@users.noreply.github.com>
2023-10-26 05:02:38 +0000

    caparision confusion

---
+++ basics/caparison-confusion.js
@@ -0,0 +1,19 @@
+// console.log(2>2);
+// console.log(2>=2);
+// console.log(2<2);
+// console.log(2<=2);
+// console.log(2==2);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+// console.log(undefined == 0);
+
+
+//Strict check  using ===
+
+console.log("2"==2); //true q k yay sirf compare kr raha  hai data type nahi check krta
+console.log("2"===2); // false q k yaha yay data type check kr raha hai jo k string hai aur dosri value number is liyay yay false hai
