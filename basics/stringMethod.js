f1be16e84cafc647bd9df9e0ffffc6d1e3d87f4f
MianAteeq-star <142322826+MianAteeq-star@users.noreply.github.com>
2023-10-26 16:05:51 +0000

    stringMethod

---
+++ basics/stringMethod.js
@@ -0,0 +1,17 @@
+const stringOne = "Mian"
+// const count = 34
+// console.log(stringOne + count +" g");
+// console.log(`My name is ${stringOne}  g and the value of count is ${count} `);
+const newString = new String("Mian-M-Ateeq")
+// console.log(newString[2]);
+console.log(newString.length);
+// console.log(newString.slice(0,3));
+// console.log( typeof newString.split("-"));
+console.log(newString);
+// console.log(newString.trimStart());
+// console.log(newString.trimEnd());
+// console.log(newString.trim());
+// console.log(newString.replace(' ', "3").repeat(13));
+// console.log(newString.anchor("ab"));
+// console.log(newString.charAt(1));
+// console.log(newString.indexOf("M"));
