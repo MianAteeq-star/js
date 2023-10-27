d1ac9676940758d93266a1c1805a36d960f96c89
MianAteeq-star <142322826+MianAteeq-star@users.noreply.github.com>
2023-10-27 06:36:05 +0000

    numsAndMaths

---
+++ basics/numsAndMaths.js
@@ -0,0 +1,21 @@
+const num= 100
+// console.log(num.toString().length);
+const number = new Number(121)
+// console.log(number);
+//  console.log(number.toExponential(4))
+// console.log(number.toLocaleString());
+// console.log(number.toPrecision(2));
+// console.log(number.valueOf(233)); not ⚒ 
+
+
+// ++++++++++++++++++++Maths+++++++++++++++++++++++
+
+console.log(Math);
+// console.log(Math.abs(-9)); make all values positive
+// console.log(Math.round((Math.random()*10)+6));
+const num1=200.6546
+// console.log(Math.ceil(num1));
+// console.log(Math.floor(num1));
+// console.log(Math.round(num1));
+console.log(Math.PI);
+console.log(Math.max(2,3,5,1,5,7,7).toString().replace("7","6"));
