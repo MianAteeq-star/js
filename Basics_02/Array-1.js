860e98c4baed0568e7ee9a546981d44cf1e44b47
MianAteeq-star <142322826+MianAteeq-star@users.noreply.github.com>
2023-10-31 04:53:07 +0000

    arrays

---
+++ Basics_02/Array-1.js
@@ -0,0 +1,19 @@
+const arr= [1,2,3,4,5,6,7]
+// console.log(arr);
+// arr.push(0)
+// console.log(arr);
+// arr.pop()
+// console.log(arr);
+// arr.shift()
+// console.log(arr);
+// arr.unshift("o")
+// console.log(arr);
+
+// const arr2 = ["Hello Mian-M-Ateeq", "hello"]
+// const arr3 = arr.concat(arr2)
+// console.log(arr3);
+console.log( "A ", arr);
+console.log(arr.slice(1,4));
+console.log( "B ", arr);
+console.log(arr.splice(1,4));
+console.log( "C ", arr);
---
+++ Basics_02/array_2.js
@@ -0,0 +1,20 @@
+const my_hero= ["Mian","g","The Great"]
+const appKHeros= ["batman","thor","Iron Man"]
+// const my_hero= ["Mian","g","The Great"]
+// my_hero.push(appKHeros)
+// console.log(my_hero);
+// const total_Heros= my_hero+ appKHeros
+// console.log(total_Heros);
+// const newHero = my_hero.concat(appKHeros)
+// console.log("without spread operator ",newHero);
+// const all = [...my_hero,...appKHeros]
+// console.log("with spread operator ",all);
+const another = [1,2,3,4, [3,5,6, [3,9,0,[1,3,4,7]],[3,2,1,5,5]]]
+console.log(another.flat(Infinity));
+console.log(Array.isArray(1));
+console.log(Array.from("Mian"));
+const score1 = 100
+const score2 = 100
+const score3 = 100
+const score4 = 100
+console.log(Array.of(score1,score2,score3,score4));
