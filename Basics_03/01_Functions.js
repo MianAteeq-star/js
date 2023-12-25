45ba8b04993b37b574d936d24ed6dbb38fd30dea
Mr Ateeq <142322826+MianAteeq-star@users.noreply.github.com>
2023-12-25 05:54:17 +0000

    Functions

---
+++ Basics_03/01_Functions.js
@@ -0,0 +1,25 @@
+function MyName () {
+    console.log("My Name is Ateeq");
+}
+
+// MyName()
+
+
+function addNo (number1,number2){
+    return number1+ number2
+}
+
+
+console.log(addNo(3,4))
+
+
+function login(username) {
+    if (!username) {
+      return  console.log("please enter userName");
+    }
+
+   return `${username} just loggedIn `
+}
+
+
+console.log(login());
