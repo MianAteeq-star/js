0d6bfd93e8a51c7dfa5c5e01b4d729953c689342
Mr Ateeq <142322826+MianAteeq-star@users.noreply.github.com>
2023-12-26 05:20:01 +0000

    array and object in function

--- Basics_03/01_Functions.js
+++ Basics_03/01_Functions.js
@@ -10,7 +10,7 @@ function addNo (number1,number2){
 }
 
 
-console.log(addNo(3,4))
+// console.log(addNo(3,4))
 
 
 function login(username) {
@@ -22,4 +22,51 @@ function login(username) {
 }
 
 
-console.log(login("Mian"));
+// console.log(login("Mian"));
+
+
+
+function calculatedPrice (...num){
+ return num
+}
+
+// console.log(calculatedPrice(200,400,500))
+
+
+
+
+function calculatedPrices(num1,num2,...num){
+ return num
+}
+
+// console.log(calculatedPrices(200,400,500,70,60))
+
+
+
+
+
+
+const obj = {
+    name:"Ateeq",
+    age:18
+}
+
+function handleObj(anyObject) {
+    return `my name is ${anyObject.name} and age is ${anyObject.age}`
+}
+
+// console.log(handleObj(obj));
+console.log(handleObj({
+    name:"mian",
+    age:18
+}));
+
+
+
+
+const my = [1,2,3,4,5]
+
+function handleArray (anyArray){
+    return anyArray
+}
+console.log(handleArray([5,6,7,8,9]));
